3b1dc2f2536cc630866fad3618b8707ef02f46df
Keerthi Kumar NARAYAN <keerthikumar.narayan@amadeus.com>
2020-07-05 19:55:32 +0530

    minor changes

--- src/main/java/com/spring/docker/demo/SpringWithDocker/SpringWithDockerApplication.java
+++ src/main/java/com/spring/docker/demo/SpringWithDocker/SpringWithDockerApplication.java
@@ -8,14 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class SpringWithDockerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringWithDockerApplication.class, args);
-	}
-
+	
 	@GetMapping("/message")
 	public String printMessage() {
 		return "Welcome to the world of spring boot with Docker !!";
 	}
 
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithDockerApplication.class, args);
+	}
 }
